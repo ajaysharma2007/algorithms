3133ae2ffd217878c3dd5ca295e513749eac13f1
Ajay <ajay@Ajays-MacBook-Pro.local>
2016-09-07 20:15:51 +0530

    adding info regarding number of guesses

--- src/main/java/search/BinarySearch.java
+++ src/main/java/search/BinarySearch.java
@@ -16,7 +16,15 @@
             ArrayIndexOutOfBound exception.
         - In this scenario if we take array as [1,2,3,4,5] then only following indexes will be covered:
             - 2, 4, 0
+ */
 
+/*
+    The logarithm function grows very slowly. Logarithms are the inverse of exponentials, which grow very rapidly, so that if lg n=x, then n = 2^x​ .
+    For example, because lg 128 = 7, we know that 2^7 = 128.
+
+    When n(number of elements in an array) is not a power of 2, we can just go up to the next higher power of 2.
+    For an array whose length is 1000, the next higher power of 2 is 1024, which equals 2^10.
+    Therefore, for a 1000-element array, binary search would require at most 11 (10 + 1) guesses.
  */
 
 package search;
