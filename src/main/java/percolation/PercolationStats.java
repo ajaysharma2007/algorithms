8e224606afb968250a8176481687b2af991da4d0
Ajay <ajaysharma2007@gmail.com>
2016-10-04 12:38:50 +0530

    PercolationStats Complete

--- src/main/java/percolation/PercolationStats.java
+++ src/main/java/percolation/PercolationStats.java
@@ -14,21 +14,54 @@ public class PercolationStats {
     double[] thresholds;
 
 
-    // perform trials independent experiments on an n-by-n grid
     public PercolationStats(int n, int trials) {
         validateInputs(n, trials);
         this.gridSize = n;
         this.numTrials = trials;
-        this.percolation = new Percolation(n);
         thresholds = new double[trials];
+
+        populateThresholdValues();
     }
 
-    // sample mean of percolation threshold
     public double mean() {
+        return StdStats.mean(thresholds);
+    }
+
+    public double stddev() {
+        if (this.numTrials <= 1) {
+            return Double.NaN;
+        }
+
+        return StdStats.stddev(this.thresholds);
+    }
+
+    public double confidenceLo() {
+        return this.mean() - ((1.96 * this.stddev()) / Math.sqrt(this.numTrials));
+    }
+
+    public double confidenceHi() {
+        return this.mean() + ((1.96 * this.stddev()) / Math.sqrt(this.numTrials));
+    }
+
+    public static void main(String[] args) {
+        PercolationStats percolationStats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        System.out.println("mean                       = " + percolationStats.mean());
+        System.out.println("stddev                     = " + percolationStats.stddev());
+        System.out.format("95%% confidence interval    = %s, %s%n", percolationStats.confidenceLo(), percolationStats.confidenceHi());
+    }
+
+    private void validateInputs(int gridSize, int numTrials) {
+        if (gridSize <= 0 || numTrials <= 0) {
+            throw new IllegalArgumentException("Grid size and Number of trials can't be less than zero");
+        }
+    }
+
+    private void populateThresholdValues() {
         for (int i = 1; i <= this.numTrials; i++) {
 
+            this.percolation = new Percolation(this.gridSize);
             int openSites = 0;
-            while (this.percolation.percolates()) {
+            while (!this.percolation.percolates()) {
 
                 int x = StdRandom.uniform(1, this.gridSize + 1);
                 int y = StdRandom.uniform(1, this.gridSize + 1);
@@ -37,44 +70,10 @@ public class PercolationStats {
                     this.percolation.open(x, y);
                     openSites++;
                 }
-
-                double threshold = openSites/Math.pow(this.gridSize, 2);
-                thresholds[i-1] = threshold;
             }
-        }
 
-        return StdStats.mean(thresholds);
-    }
-
-    // sample standard deviation of percolation threshold
-    public double stddev() {
-        if(this.numTrials <= 1) {
-            return Double.NaN;
-        }
-
-
-    }
-
-//    // low  endpoint of 95% confidence interval
-//    public double confidenceLo() {
-//
-//    }
-//
-//    // high endpoint of 95% confidence interval
-//    public double confidenceHi() {
-//
-//    }
-
-    public static void main(String[] args) {
-        PercolationStats percolationStats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-
-
-
-    }
-
-    private void validateInputs(int gridSize, int numTrials) {
-        if (gridSize <= 0 || numTrials <= 0) {
-            throw new IllegalArgumentException("Grid size and Number of trials can't be less than zero");
+            double threshold = openSites / Math.pow(this.gridSize, 2);
+            thresholds[i - 1] = threshold;
         }
     }
 }
