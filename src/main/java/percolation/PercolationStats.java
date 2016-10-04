6e7ff931d8244100b9b07ccbc8b3f7fc7a6f1704
Ajay <ajaysharma2007@gmail.com>
2016-10-04 15:59:56 +0530

    Modify based on checkstyle plugin

--- src/main/java/percolation/PercolationStats.java
+++ src/main/java/percolation/PercolationStats.java
@@ -4,13 +4,17 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 /**
- * The {@code PercolationStats} class represents a <em>PercolationStats data type</em>.
- * It supports the <em>mean</em>, <em>stddev</em>, <em>confidenceLo</em> operation and a <em>confidenceHi</em> operation.
+ * The {@code PercolationStats} class represents a
+ * <em>PercolationStats data type</em>.
+ * It supports the <em>mean</em>, <em>stddev</em>, <em>confidenceLo</em>
+ * operation and a <em>confidenceHi</em> operation.
  * <ul>
- * <li><em>mean</em>(<em>i</em>, <em>j</em>) returns the mean of percolation thresholds.
- * Percolation threshold is calculated using the ratio of number of open sites required for a system to percolate to the
+ * <li><em>mean</em> returns the mean of percolation thresholds.
+ * Percolation threshold is calculated using the ratio of number of
+ * open sites required for a system to percolate to the
  * total number of sites in the grid {@code n^2}.
- * This is performed {@code t} times and then an average of all the percolation thresholds is taken and returned.
+ * This is performed {@code t} times and then an average of
+ * all the percolation thresholds is taken and returned.
  * <li><em>stddev</em> returns the standard deviation of percolation thresholds.
  * <li><em>confidenceLo</em> returns the low endpoint of 95% confidence interval.
  * <li><em>confidenceHi</em>() returns the high endpoint of 95% confidence interval.
@@ -32,7 +36,8 @@ public class PercolationStats {
      * <p>
      *
      * @param n      the size representing the n*n grid.
-     * @param trials representing the number of times experiment needs to be performed.
+     * @param trials representing the number of times
+     *               experiment needs to be performed.
      * @throws IllegalArgumentException if {@code n < 0} {@code trials < 0}
      *                                  </p>
      */
@@ -46,14 +51,16 @@ public class PercolationStats {
     }
 
     /**
-     * Returns the mean of percolation thresholds for {@code n*n} grid calculated {@code numTrials} times.
+     * Returns the mean of percolation thresholds
+     * for {@code n*n} grid calculated {@code numTrials} times.
      */
     public double mean() {
         return StdStats.mean(thresholds);
     }
 
     /**
-     * Returns the standard deviation of percolation thresholds for {@code n*n} grid calculated {@code numTrials} times.
+     * Returns the standard deviation of percolation thresholds
+     * for {@code n*n} grid calculated {@code numTrials} times.
      */
     public double stddev() {
         if (this.numTrials <= 1) {
@@ -64,37 +71,47 @@ public class PercolationStats {
     }
 
     /**
-     * Returns the low  endpoint of 95% confidence interval for {@code n*n} grid calculated {@code numTrials} times.
+     * Returns the low  endpoint of 95% confidence interval
+     * for {@code n*n} grid calculated {@code numTrials} times.
      */
     public double confidenceLo() {
         return this.mean() - ((1.96 * this.stddev()) / Math.sqrt(this.numTrials));
     }
 
     /**
-     * Returns the high  endpoint of 95% confidence interval for {@code n*n} grid calculated {@code numTrials} times.
+     * Returns the high  endpoint of 95% confidence interval
+     * for {@code n*n} grid calculated {@code numTrials} times.
      */
     public double confidenceHi() {
         return this.mean() + ((1.96 * this.stddev()) / Math.sqrt(this.numTrials));
     }
 
     public static void main(String[] args) {
-        PercolationStats percolationStats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-        System.out.println("mean                       = " + percolationStats.mean());
-        System.out.println("stddev                     = " + percolationStats.stddev());
-        System.out.format("95%% confidence interval    = %s, %s%n", percolationStats.confidenceLo(), percolationStats.confidenceHi());
+        PercolationStats percolationStats =
+                new PercolationStats(Integer.parseInt(args[0]),
+                        Integer.parseInt(args[1]));
+        System.out.println("mean                       " +
+                "= " + percolationStats.mean());
+        System.out.println("stddev                     " +
+                "= " + percolationStats.stddev());
+        System.out.format("95%% confidence interval    " +
+                        "= %s, %s%n", percolationStats.confidenceLo(),
+                percolationStats.confidenceHi());
     }
 
     /*
         Validates the inputs provided to the PercolationStats object.
      */
-    private void validateInputs(int gridSize, int numTrials) {
-        if (gridSize <= 0 || numTrials <= 0) {
-            throw new IllegalArgumentException("Grid size and Number of trials can't be less than zero");
+    private void validateInputs(int size, int trials) {
+        if (size <= 0 || trials <= 0) {
+            throw new IllegalArgumentException(
+                    "Grid size and Number of trials can't be less than zero");
         }
     }
 
     /*
-       Initializes the Percolation object and populates the threshold array for n*n grid, numTrials number of times.
+       Initializes the Percolation object and populates
+       the threshold array for n*n grid, numTrials number of times.
     */
     private void populateThresholdValues() {
         for (int i = 1; i <= this.numTrials; i++) {
