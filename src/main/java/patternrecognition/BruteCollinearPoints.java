164b1f25cb6642d0bff50c03bc62911d2bc5d44d
ajaysharma2007 <ajaysharma2007@gmail.com>
2016-10-24 09:33:55 +0530

    removing test main

--- src/main/java/patternrecognition/BruteCollinearPoints.java
+++ src/main/java/patternrecognition/BruteCollinearPoints.java
@@ -29,37 +29,6 @@ public class BruteCollinearPoints {
         collinearLineSegArr = new LineSegment[pointSet.length];
     }
 
-    public static void main(String[] args) {
-
-        // read the n points from a file
-        In in = new In("/Users/ajay/Downloads/Coursera/Algorithms Part I" +
-                "/Week-3/1-Merge Sort/Assignment/collinear/rs1423.txt");
-        int n = in.readInt();
-        Point[] points = new Point[n];
-        for (int i = 0; i < n; i++) {
-            int x = in.readInt();
-            int y = in.readInt();
-            points[i] = new Point(x, y);
-        }
-
-        // draw the points
-        StdDraw.enableDoubleBuffering();
-        StdDraw.setXscale(0, 32768);
-        StdDraw.setYscale(0, 32768);
-        for (Point p : points) {
-            p.draw();
-        }
-        StdDraw.show();
-
-        // print and draw the line segments
-        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
-        for (LineSegment segment : collinear.segments()) {
-            StdOut.println(segment);
-            segment.draw();
-        }
-        StdDraw.show();
-    }
-
     private void resize(int capacity) {
         LineSegment[] newItemArr = new LineSegment[capacity];
         for (int i = 0; i < this.collinearLineSegArr.length; i++) {
--- src/main/java/patternrecognition/FastCollinearPoints.java
+++ src/main/java/patternrecognition/FastCollinearPoints.java
@@ -29,37 +29,6 @@ public class FastCollinearPoints {
         collinearLineSegArr = new LineSegment[pointSet.length];
     }     // finds all line segments containing 4 or more points
 
-    public static void main(String[] args) {
-
-        // read the n points from a file
-        In in = new In("/Users/ajay/Downloads/Coursera/Algorithms Part I" +
-                "/Week-3/1-Merge Sort/Assignment/collinear/input20.txt");
-        int n = in.readInt();
-        Point[] points = new Point[n];
-        for (int i = 0; i < n; i++) {
-            int x = in.readInt();
-            int y = in.readInt();
-            points[i] = new Point(x, y);
-        }
-
-        // draw the points
-        StdDraw.enableDoubleBuffering();
-        StdDraw.setXscale(0, 32768);
-        StdDraw.setYscale(0, 32768);
-        for (Point p : points) {
-            p.draw();
-        }
-        StdDraw.show();
-
-        // print and draw the line segments
-        FastCollinearPoints collinear = new FastCollinearPoints(points);
-        for (LineSegment segment : collinear.segments()) {
-            StdOut.println(segment);
-            segment.draw();
-        }
-        StdDraw.show();
-    }
-
     private void resize(int capacity) {
         LineSegment[] newItemArr = new LineSegment[capacity];
         for (int i = 0; i < this.collinearLineSegArr.length; i++) {
@@ -74,6 +43,7 @@ public class FastCollinearPoints {
 
     public LineSegment[] segments() {
         for (int k = 0; k < pointSet.length - 3; k++) {
+            Arrays.sort(pointSet);
             Point point = pointSet[k];
             Arrays.sort(pointSet, k + 1, pointSet.length, point.slopeOrder());
             int i = k + 1;
