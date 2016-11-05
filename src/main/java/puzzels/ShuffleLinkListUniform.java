dce04658cb4224e6591be91a87912acd6dbbf33b
Ajay <ajaysharma2007@gmail.com>
2016-11-05 07:05:48 +0530

    shuffle linked list uniformly. IN progress

--- src/main/java/puzzels/ShuffleLinkListUniform.java
+++ src/main/java/puzzels/ShuffleLinkListUniform.java
@@ -1,43 +1,59 @@
 package puzzels;
 
-import edu.princeton.cs.algs4.StdRandom;
-
 /**
  * Created by ajay on 10/27/16.
  */
 public class ShuffleLinkListUniform {
-    private class Node {
-        private Object item;
-        private Node next;
+
+    public static void main(String[] args) {
+        ShuffleLinkListUniform s = new ShuffleLinkListUniform();
+        Node na = s.new Node(1);
+        Node nb = s.new Node(2);
+        na.next = nb;
+        Node nc = s.new Node(3);
+        nb.next = nc;
+        Node nd = s.new Node(4);
+        nc.next = nd;
+//        Node ne = s.new Node(5);
+//        nd.next = ne;
+//        Node nf = s.new Node(6);
+//        ne.next = nf;
+//        Node ng = s.new Node(7);
+//        nf.next = ng;
+//        Node nh = s.new Node(8);
+//        ng.next = nh;
+//        Node nj = s.new Node(9);
+//        nh.next = nj;
+
+        System.out.println("Before shuffle:");
+        s.shuffle(na, 9);
+        System.out.println("After shuffle:");
+        s.print(na);
     }
 
     private void merge(Node lh, Node rh, int leftSize, int rightSize) {
         Node left = lh;
         Node right = rh;
 
-        if (StdRandom.uniform(1) > 0) {
-            lh = right;
-            right = right.next;
-        } else {
-            left = left.next;
-        }
+        Node runner = new Node(0);
 
-        Node runner = lh;
+        int i = 1, j = 1;
 
-        while (right != null || left != null) {
-            if (left == null) {
-                runner.next = right;
-                right = right.next;
-            } else if (right == null) {
+        while (i <= leftSize || j <= rightSize) {
+            int remainingLeftItems = leftSize - i + 1;
+            int remainingRightItems = leftSize - j + 1;
+
+            if (Math.random() < remainingLeftItems /
+                    (remainingLeftItems + remainingRightItems)) {
                 runner.next = left;
                 left = left.next;
-            } else if (StdRandom.uniform(1) > 0) {
-                runner.next = right;
-                right = right.next;
+                i = i + 1;
             } else {
-                runner.next = left;
-                left = left.next;
+                runner.next = right;
+                right = right.next;
+                j = j + 1;
             }
+
             runner = runner.next;
         }
     }
@@ -53,10 +69,29 @@ public class ShuffleLinkListUniform {
         }
         Node rh = mid.next;
         mid.next = null;
-        int leftSize = N/2;
-        int rightSize = N - N/2;
+        int leftSize = N / 2;
+        int rightSize = N - N / 2;
         shuffle(head, N / 2);
         shuffle(rh, N - N / 2);
         merge(head, rh, leftSize, rightSize);
     }
+
+    public void print(Node n) {
+        Node curr = n;
+        while (curr != null) {
+            System.out.print(curr.val + " -> ");
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+
+    private class Node {
+        private int val;
+        private Node next;
+
+        Node(int value) {
+            this.val = value;
+        }
+    }
+
 }
