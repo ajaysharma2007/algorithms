2ecf1c46a4383cfff5f99bb9fdb1015c20ca8913
ajaysharma2007 <ajaysharma2007@gmail.com>
2016-11-08 14:44:50 +0530

    Solver implementation

--- src/main/java/eight_puzzle/Solver.java
+++ src/main/java/eight_puzzle/Solver.java
@@ -1,16 +1,33 @@
 package eight_puzzle;
 
+import edu.princeton.cs.algs4.MinPQ;
+
 /**
  * Created by ajay on 11/8/16.
  */
 public class Solver {
-    public Solver(Board initial)           // find a solution to the initial board (using the A* algorithm)
 
+    private Board initialBoard;
+    private int moves = 0;
+
+    public Solver(Board initial) {
+        this.initialBoard = initial;
+    }        // find a solution to the initial board (using the A* algorithm)
+
+    private void solveBoard() {
+        MinPQ<Board>
+    }
     public boolean isSolvable()            // is the initial board solvable?
 
-    public int moves()                     // min number of moves to solve initial board; -1 if unsolvable
+    public int moves() {
 
-    public Iterable<Board> solution()      // sequence of boards in a shortest solution; null if unsolvable
+        return -1;
+    }                 // min number of moves to solve initial board; -1 if unsolvable
+
+    public Iterable<Board> solution() {
+
+        return null;
+    }      // sequence of boards in a shortest solution; null if unsolvable
 
     public static void main(String[] args) // solve a slider puzzle (given below)
 }
